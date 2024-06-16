8d1f22ed0593de7a2ce95cdea7af61606b3e4454
Wesley Farias <wesley_Farias7@outlook.com>
2024-06-16 14:11:14 -0300

    primeiro-commit

---
+++ script.js
@@ -0,0 +1,20 @@
+let slideIndex = [1,1];
+let slideClassNames = ["slide", "slide2"]
+showSlides(1, 0);
+showSlides(1, 1);
+
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
+}
+
+function showSlides(n, no) {
+  let i;
+  let x = document.getElementsByClassName(slideClassNames[no]);
+  if (n > x.length) {slideIndex[no] = 1}    
+  if (n < 1) {slideIndex[no] = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  x[slideIndex[no]-1].style.display = "block";  
+}
+
